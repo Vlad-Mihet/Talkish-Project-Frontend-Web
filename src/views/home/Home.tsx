21f6ed22cb26c235c9b605355a1e1c8ab6bd1de9
Vlad Mihet <vlad.mihet@amdaris.com>
2021-11-28 19:20:36 +0200

    feat: created BlogsContainer component

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,34 +1,11 @@
-import { Suspense, useEffect } from 'react';
+import { Suspense } from 'react';
 import Layout from 'src/layout/components/Layout';
-import { Blog } from '../../components';
 import Sidebar from '../../layout/components/Sidebar';
-import useFetchData from '../../hooks/useFetchData';
-import { Endpoints } from '../../config';
-import type { BlogWithAuthor as BlogType } from '../../types';
 import RecommendedTopics from '../../layout/components/RecommendedTopics';
 import RecommendedAuthors from '../../layout/components/RecommendedAuthors';
+import { BlogsContainer } from './components'
 
 export default function Home(): JSX.Element {
-  // Fetch all blog data here
-  const fetchAllBlogsUrl = Endpoints.ROOT + '/' + Endpoints.BLOGS;
-
-  const {
-    data: blogs,
-    loading: blogsLoading,
-    error: blogsError,
-  } = useFetchData(fetchAllBlogsUrl);
-
-  /*
-    TODO:
-      Banner Component for errors
-  */
-
-  useEffect(() => {
-    if (!blogsLoading && blogsError) {
-      console.error(`There was an issue: ${blogsError}`);
-    }
-  }, [blogsLoading, blogsError]);
-
   return (
     <Layout
       sidebar={
@@ -46,24 +23,9 @@ export default function Home(): JSX.Element {
         />
       }
     >
-      {blogsLoading
-        ? (<span>Loading data...</span>)
-        : (
-          <div>
-            {blogs && blogs.map((blog: BlogType) => (
-              <Blog
-                key={blog.blogId}
-                blogId={blog.blogId}
-                title={blog.title}
-                content={blog.content}
-                topics={blog.topics}
-                readingTime={blog.readingTime}
-                authorId={blog.author.authorId}
-                authorName={blog.author.authorName}
-              />
-            ))}
-          </div>
-        )}
+      <Suspense fallback={<p>Loading Data...</p>}>
+        <BlogsContainer />
+      </Suspense>
     </Layout>
   )
 }
---
+++ src/views/home/components/BlogsContainer/BlogsContainer.tsx
@@ -0,0 +1,38 @@
+import { Blog } from '../../../../components';
+import { Endpoints } from '../../../../config';
+import type { BlogWithAuthor as BlogType } from '../../../../types';
+import useFetchData from '../../../../hooks/useFetchData';
+import { useEffect } from 'react';
+
+const fetchAllBlogsUrl = Endpoints.ROOT + '/' + Endpoints.BLOGS;
+
+export default function BlogsContainer() {
+  const {
+    data: blogs,
+    loading: blogsLoading,
+    error: blogsError,
+  } = useFetchData(fetchAllBlogsUrl);
+
+  useEffect(() => {
+    if (!blogsLoading && blogsError) {
+      console.error(`There was an issue: ${blogsError}`);
+    }
+  }, [blogsLoading, blogsError]);
+
+  return (
+    <div>
+      {blogs && blogs.map((blog: BlogType) => (
+        <Blog
+          key={blog.blogId}
+          blogId={blog.blogId}
+          title={blog.title}
+          content={blog.content}
+          topics={blog.topics}
+          readingTime={blog.readingTime}
+          authorId={blog.author.authorId}
+          authorName={blog.author.authorName}
+        />
+      ))}
+    </div>
+  )
+}
