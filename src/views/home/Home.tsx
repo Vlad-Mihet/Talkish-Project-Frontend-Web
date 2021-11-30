b7d775f3f58bf81686e2b8d617166ac81706394a
Vlad Mihet <61595636+Vlad-Mihet@users.noreply.github.com>
2021-11-30 15:23:58 +0200

    refactor: remove TopicDataShowcase comp from Home after testing

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -2,7 +2,7 @@ import { Suspense } from 'react';
 import Layout from 'src/layout/Layout';
 import Sidebar from '../../layout/components/Sidebar';
 import {
-  RecommendedTopics, RecommendedAuthors, TopicDataShowcase
+  RecommendedTopics, RecommendedAuthors,
 } from '../../layout/components';
 import { BlogsContainer } from './components'
 
@@ -19,14 +19,6 @@ export default function Home(): JSX.Element {
           middleSection={
             <Suspense fallback={<p>Loading Data...</p>}>
               <RecommendedAuthors />
-            </Suspense>
-          }
-          bottomSection={
-            <Suspense fallback={<p>Loading Data...</p>}>
-              <TopicDataShowcase
-                numberOfAuthors={32151}
-                numberOfStories={534263}
-              />
             </Suspense>
           }
         />
