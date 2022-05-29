a49195bbb41fb582a00f625ddee5f915d2ea0d70
Vlad Mihet <vlad.mihet@amdaris.com>
2022-05-29 20:12:28 +0300

    feat: setup redux store config

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,16 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { reducer as authReducer } from './auth/reducer';
 import thunk from 'redux-thunk';
 
-const reducers = combineReducers({});
+import type { AnyAction } from 'redux';
+import type { ThunkDispatch } from 'redux-thunk';
 
-export default createStore(reducers, applyMiddleware(thunk));
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
+
+export type TAppState = ReturnType<typeof rootReducer>;
+export type TDispatch = ThunkDispatch<TAppState, void, AnyAction>;
+
+export default createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
